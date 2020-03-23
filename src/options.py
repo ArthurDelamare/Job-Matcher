face86729a69aeeed1ae96d881a94c559750943e
Unknown <arthur.delamare1@viacesi.fr>
2020-03-23 16:18:07 +0100

    add(options): domain keywords typing

--- src/options.py
+++ src/options.py
@@ -1,5 +1,7 @@
+from typing import List
+
 class Options: 
 
-    def __init__(self, domain_keywords = [], sections = False):
+    def __init__(self, domain_keywords: List[str] = [], sections = False):
         self.domain_keywords = domain_keywords
         self.sections = sections
