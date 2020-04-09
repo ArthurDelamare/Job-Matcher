7d02af3aec575c64a2952325e53cea8ad230930d
Unknown <arthur.delamare1@viacesi.fr>
2020-04-10 00:46:26 +0200

    feat(resume): compare function for general keywords

--- src/resume.py
+++ src/resume.py
@@ -1,4 +1,16 @@
 from .document import Document
+from .position import Position
 
 class Resume(Document):
-    pass
+    
+    def compare(self, position: Position):
+        missing_keywords = dict()
+        matching_keywords = dict()
+
+        d = [x.lower() for x in self.keywords] # make dict of list with less elements  
+        for key, value in position.keywords.items():  # search against bigger list  
+            if key.lower() in d:
+                matching_keywords[key] = value
+            else: 
+                missing_keywords[key] = value
+        return matching_keywords, missing_keywords
--- src/test.py
+++ src/test.py
@@ -76,5 +76,65 @@ class TestProject(unittest.TestCase):
         self.assertEqual(position.has_section(Responsibilities.name.lower()), True)
         self.assertEqual(position.has_section(Volunteering.name.lower()), False)
 
+
+    def test_compare(self):
+        resume_text = '''
+            Name : John Doe
+            Title : Software Engineer
+
+            Summary :
+            My name is John Doe, I am software engineer with 3 years experience. Especially comfortable in web development, I'm open to job offers.
+            
+            Skills : 
+            Java, Python, Php, JavaScript, TypeScript, Golang
+            
+            Education :
+            2016 - 2021 : Master in Computer Science, Random School
+
+            Experience :
+            2018 - Now : Software Engineer at My Dream Company
+
+            2016 - 2018 : Frontend Developer at Random CMS Company
+
+            Volunteering :
+            2017 - Now : Equal right activist
+        '''
+
+        position_text = '''
+            We are looking for an enthusiastic Junior Software Developer to join our experienced software design team. You will report directly to the Development Manager and assist with all functions of software coding and design. Your primary focus will be to learn the codebase, gather user data, and respond to requests from senior developers.
+
+            To ensure success as a Junior Software Developer, you should have a good working knowledge of basic programming languages, the ability to learn new technology quickly, and the ability to work in a team environment. Ultimately, a top-class Junior Software Developer provides valuable support to the design team while continually improving their coding and design skills.
+
+            Responsibilities:
+            Assisting the Development Manager with all aspects of software design and coding.
+            Attending and contributing to company development meetings.
+            Learning the codebase and improving your coding skills.
+            Writing and maintaining code.
+            Working on minor bug fixes.
+            Monitoring the technical performance of internal systems.
+            Responding to requests from the development team.
+            Gathering information from consumers about program functionality.
+            Writing reports.
+            Conducting development tests.
+            
+            Requirements:
+            Bachelor's degree in Computer Science.
+            Knowledge of basic coding languages including C++, HTML5, and JavaScript.
+            Basic programming experience.
+            Knowledge of databases and operating systems.
+            Good working knowledge of email systems and Microsoft Office software.
+            Ability to learn new software and technologies quickly.
+            Ability to follow instructions and work in a team environment.
+            Detail-oriented.
+        '''
+
+        resume = Resume(resume_text)
+        position = Position(position_text)
+        matching, missing = resume.compare(position)
+        self.assertEqual('software' in matching.keys(), True)
+        self.assertEqual('react' in matching.keys(), False)
+        self.assertEqual('HTML5' in missing.keys(), True)
+        self.assertEqual('C++' in missing.keys(), True)
+
 if __name__ == "__main__":
     unittest.main()
