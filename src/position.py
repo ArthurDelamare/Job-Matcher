36c84c3eb9a9d9d11de7d9775844eb6d78640f63
Unknown <arthur.delamare1@viacesi.fr>
2020-04-10 22:52:13 +0200

    feat(position): compare function for general and domain keywords

--- src/position.py
+++ src/position.py
@@ -1,4 +1,27 @@
 from .document import Document
 
 class Position(Document):
-    pass
+    
+    def compare_global_keywords(self, resume: 'Resume'):
+        missing_keywords = dict()
+        matching_keywords = dict()
+
+        d = [x.lower() for x in resume.keywords] # make dict of list with less elements  
+        for key, value in self.keywords.items():  # search against bigger list  
+            if key.lower() in d:
+                matching_keywords[key] = value
+            else: 
+                missing_keywords[key] = value
+        return matching_keywords, missing_keywords
+
+    def compare_domain_keywords(self, resume: 'Resume'):
+        missing_keywords = list()
+        matching_keywords = list()
+
+        d = [domain_keyword.lower() for domain_keyword in resume.domains_keywords] # make dict of list with less elements  
+        for value in self.domains_keywords:  # search against bigger list  
+            if value.lower() in d:
+                matching_keywords.append(value)
+            else: 
+                missing_keywords.append(value)
+        return matching_keywords, missing_keywords
--- src/test.py
+++ src/test.py
@@ -77,7 +77,7 @@ class TestProject(unittest.TestCase):
         self.assertEqual(position.has_section(Volunteering.name.lower()), False)
 
 
-    def test_compare(self):
+    def test_resume_compare(self):
         resume_text = '''
             Name : John Doe
             Title : Software Engineer
@@ -139,6 +139,71 @@ class TestProject(unittest.TestCase):
         self.assertEqual('C++' in missing_general_keywords.keys(), True)
 
         matching_domain_keywords, missing_domain_keywords = resume.compare_domain_keywords(position)
+        self.assertEqual('JavaScript' in matching_domain_keywords, True)
+        self.assertEqual('C++' in missing_domain_keywords, True)
+
+    def test_position_compare(self):
+        resume_text = '''
+            Name : John Doe
+            Title : Software Engineer
+
+            Summary :
+            My name is John Doe, I am software engineer with 3 years experience. Especially comfortable in web development, I'm open to job offers.
+            
+            Skills : 
+            Java, Python, Php, JavaScript, TypeScript, Golang
+            
+            Education :
+            2016 - 2021 : Master in Computer Science, Random School
+
+            Experience :
+            2018 - Now : Software Engineer at My Dream Company
+
+            2016 - 2018 : Frontend Developer at Random CMS Company
+
+            Volunteering :
+            2017 - Now : Equal right activist
+        '''
+
+        position_text = '''
+            We are looking for an enthusiastic Junior Software Developer to join our experienced software design team. You will report directly to the Development Manager and assist with all functions of software coding and design. Your primary focus will be to learn the codebase, gather user data, and respond to requests from senior developers.
+
+            To ensure success as a Junior Software Developer, you should have a good working knowledge of basic programming languages, the ability to learn new technology quickly, and the ability to work in a team environment. Ultimately, a top-class Junior Software Developer provides valuable support to the design team while continually improving their coding and design skills.
+
+            Responsibilities:
+            Assisting the Development Manager with all aspects of software design and coding.
+            Attending and contributing to company development meetings.
+            Learning the codebase and improving your coding skills.
+            Writing and maintaining code.
+            Working on minor bug fixes.
+            Monitoring the technical performance of internal systems.
+            Responding to requests from the development team.
+            Gathering information from consumers about program functionality.
+            Writing reports.
+            Conducting development tests.
+            
+            Requirements:
+            Bachelor's degree in Computer Science.
+            Knowledge of basic coding languages including C++, HTML5, and JavaScript.
+            Basic programming experience.
+            Knowledge of databases and operating systems.
+            Good working knowledge of email systems and Microsoft Office software.
+            Ability to learn new software and technologies quickly.
+            Ability to follow instructions and work in a team environment.
+            Detail-oriented.
+        '''
+
+        options = Options(domain_keywords = ['JavaScript', 'C++', 'HTML5'])
+
+        resume = Resume(text = resume_text, options = options)
+        position = Position(text = position_text, options = options)
+        matching_general_keywords, missing_general_keywords = position.compare_global_keywords(resume)
+        self.assertEqual('software' in matching_general_keywords.keys(), True)
+        self.assertEqual('react' in matching_general_keywords.keys(), False)
+        self.assertEqual('HTML5' in missing_general_keywords.keys(), True)
+        self.assertEqual('C++' in missing_general_keywords.keys(), True)
+
+        matching_domain_keywords, missing_domain_keywords = position.compare_domain_keywords(resume)
         self.assertEqual('JavaScript' in matching_domain_keywords, True)
         self.assertEqual('C++' in missing_domain_keywords, True)
 
